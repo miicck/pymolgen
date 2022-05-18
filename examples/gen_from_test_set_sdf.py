c8bd73894c8827c595377386c3bf5b10c25b6ed7
Bruno <bruno.falconepin@nottingham.ac.uk>
2022-05-18 10:30:46 +0100

    Added molecular_weight function to Molecule class

--- examples/gen_from_test_set_sdf.py
+++ examples/gen_from_test_set_sdf.py
@@ -19,7 +19,8 @@ random.seed(100)
 for mol, fragments in generate_from_molecules(
         dataset, lambda m: m.atom_count > 50, MaxOrderBondGenerator(), max_frag_size=20):
     smi = molecule_to_smiles(mol)
-    print(smi)
+    mw = '%.1f' %Molecule.molecular_weight(mol)
+    print(smi, mw)
     try:
         plot_molecule(mol, title=f"Molecule from {fragments} fragments")
         #plot_molecule_graph(mol)
