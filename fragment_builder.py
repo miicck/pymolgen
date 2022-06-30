031a76690a63cdc973e1596f6270a02b7efbb2be
Bruno Falcone <bnfalcone@yahoo.es>
2022-07-01 00:28:33 +0100

    Working script

--- fragment_builder.py
+++ fragment_builder.py
@@ -172,7 +172,7 @@ def reverse_canonical_mapping(fragment):
 
     return canonical_mapping
 
-def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, parent_fragment_file, remove_hydrogens, remove_hydrogens_parent_fragment, outfile_name):
+def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, parent_fragment_file, remove_hydrogens, remove_hydrogens_parent_fragment, parent_mapping, outfile_name):
 
     #make databases and update atom numberings
     fragment_database = get_fragment_database(fragments_sdf)
@@ -180,22 +180,16 @@ def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, p
     bond_frequencies = get_bond_frequencies(frequencies_txt)   
     bond_frequencies = update_bond_frequencies(bond_frequencies, frag_mapping)
 
-    for key, val in bond_frequencies.items():
-        if key[0] == 4 and key[1] == 23:
-            print(key, val)
+    parent_mol = molecule_from_sdf(parent_file)
 
-    print_molecule(fragment_database[4])
-    print_molecule(fragment_database[23])
+    for i in remove_hydrogens:
+        parent_mol = parent_mol.remove_atom(i)
 
-    mol = molecule_from_sdf(parent_file)
+    parent_mw = Molecule.molecular_weight(parent_mol)
 
-    parent_mw = Molecule.molecular_weight(mol)
-
-    mw = parent_mw
-
-    #prepare parent
+    #prepare parent fragment
     frag_list = []
-    frag_mol_list = []
+    frag_mol_list = [parent_mol]
     frag_bond_list = []
     frag_free_valence_list = []
 
@@ -212,13 +206,14 @@ def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, p
     print('parent_fragment')
     print_molecule(parent_fragment)
     print('parent_fragment.free_valence_list =', parent_fragment.free_valence_list)
-    for i in parent_fragment.free_valence_list:
+
+    for i in parent_mol.free_valence_list:
         frag_free_valence_list[0].append(i)
 
     if parent_fragment_i is False:
         sys.exit('Parent fragment not found')
 
-    frag_list.append(parent_fragment_i)
+    frag_list.append(-1)
     print('start frag list =', frag_list)
 
     counter = 0
@@ -242,18 +237,30 @@ def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, p
             # get fragment_i (index in fragment_database)
             fragment_i = frag_list[i]
 
-            # get mol for fragment_i
-            fragment_i_mol = fragment_database[fragment_i]
+            if fragment_i == -1:
+                fragment_i = parent_fragment_i
+                print('parent_fragment_i =', parent_fragment_i)
+                
+                # get mol for fragment_i
+                fragment_i_mol = fragment_database[fragment_i]
 
-            # get canonical mapping
-            canonical_mapping = get_canonical_mapping(fragment_i_mol.graph)
+                # get mapped atom_i since fragment_bond_frequencies are stored for canonical atoms
+                atom_i_can = 2
 
-            # get mapped atom_i since fragment_bond_frequencies are stored for canonical atoms
-            atom_i_can = canonical_mapping[atom_i]
+
+            else:
+                # get mol for fragment_i
+                fragment_i_mol = fragment_database[fragment_i]
+
+                # get canonical mapping
+                canonical_mapping = get_canonical_mapping(fragment_i_mol.graph)
+
+                # get mapped atom_i since fragment_bond_frequencies are stored for canonical atoms
+                atom_i_can = canonical_mapping[atom_i]
 
             # get bond frequencies for fragment_i
             fragment_bond_frequencies = get_fragment_bond_frequencies(fragment_i, atom_i_can, bond_frequencies)
-            
+            print('frag bond freq=', fragment_bond_frequencies)        
             # choose random neighbour
             new_frag_i, new_frag_i_atom = get_random_neighbour(fragment_i, fragment_bond_frequencies)
             print('neighbour =', new_frag_i, new_frag_i_atom)
@@ -290,22 +297,19 @@ def build_molecule(fragments_sdf, fragments_txt, frequencies_txt, parent_file, p
     print('frag_list =', frag_list)
     print('frag_bond_list =', frag_bond_list)
 
-    for i in frag_list:
+    for i in frag_list[1:]:
         frag_mol_list.append(fragment_database[i])
 
     mol = combine_all_fragments(frag_mol_list, frag_list, frag_bond_list)
-
-    for i in mol.graph.nodes:
-        print(mol.graph.nodes[i])
-
-    print_molecule(mol)
 
     lines = molecule_to_sdf(mol)
 
     with open(outfile_name, 'w') as outfile:
         for line in lines:
             outfile.write(line)
+
         outfile.write('$$$$')
+
 
 def combine_all_fragments(frag_mol_list, frag_list, frag_bond_list):
 
@@ -351,62 +355,13 @@ def combine_all_fragments(frag_mol_list, frag_list, frag_bond_list):
 
     return mol
 
-def combine_two_fragments(
-        molecule_a,
-        molecule_b,
-        a_point,
-        b_point,
-        name_a,
-        name_b):
-    """
-    Given two molecules, attempt to glue them together by creating a bond
-    between the attachment point of molecule_a and free valence points of molecule_b. 
-    Returns None if no compatible valence points exist.
-
-    Parameters
-    ----------
-    molecule_a
-        First molecule
-    molecule_b
-        Second molecule
-    atom_a
-        Attachment point of molecule_a
-    atom_b
-        Attachment point of molecule_b
-    Returns
-    -------
-    None if molecules could not be glued together, otherwise
-    the newly-created, glued-together molecule.
-    """
-
-    if len(molecule_a.attach_points) == 0:
-        return None
-    if len(molecule_b.attach_points) == 0:
-        return None
-
-    # Create a copy of molecule b that does not share node ids with molecule a 
-    # and update b_point with new numbering
-    molecule_b = molecule_b.copy()
-    #molecule_b.make_disjoint(molecule_a)
-    #b_point += len(molecule_a.nodes)
-
-    # Build the glued-together molecule
-    mol = Molecule()
-
-    assert a_point in molecule_a.graph.nodes
-    assert b_point in molecule_b.graph.nodes
-    
-    mol.graph = networkx.union(molecule_b.graph, molecule_a.graph, rename=('%s-' %name_a, '%s-' %name_b))
-
-    return mol
-
 if __name__ == '__main__':
 
     #random.seed(100)
 
     outfile_name = sys.argv[1]
 
-    build_molecule('fragments10-2.sdf', 'fragments10-2.txt', 'frequencies10-2.txt', 'zgwhxzahbbyfix-26.sdf', 'methane.sdf', [26], [3], outfile_name)
+    build_molecule('fragments1000.sdf', 'fragments1000.txt', 'frequencies1000.txt', 'zgwhxzahbbyfix-26.sdf', 'ammonia-3.sdf', [26], [3], {5:2}, outfile_name)
 
 
 
