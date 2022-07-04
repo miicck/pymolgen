3b502a18d9d6f0c5c6e48a4e781201364246731b
Bruno Falcone <bnfalcone@yahoo.es>
2022-07-04 22:24:41 +0100

    run

--- fragment_builder.py
+++ fragment_builder.py
@@ -429,7 +429,7 @@ if __name__ == '__main__':
 
     outfile_name = sys.argv[1]
 
-    build_molecule('fragments.sdf', 'fragments.txt', 'frequencies.txt', 'zgwhxzahbbyfix-26.sdf', 'zgwhxzahbbyfix-26-amide-6.sdf', [26], [6], {5:2}, outfile_name, 100, filters=True, unique=True, figure='fig.sdf')
+    build_molecule('fragments.sdf', 'fragments.txt', 'frequencies.txt', 'isoxazole-13.sdf', 'isoxazole-13.sdf', [13], [100], {5:2}, outfile_name, 100, filters=True, unique=True, figure='fig.sdf')
 
 
 
